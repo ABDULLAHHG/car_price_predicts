251bb2421510d7b186cc389d1af980a1b0e45b96
Aboud <gh.abd.com.207@gmail.com>
2023-07-12 19:55:00 +0300

    clean code

--- mian.py
+++ mian.py
@@ -20,11 +20,12 @@ df.engine_capacity = df.engine_capacity.apply(lambda x :x.replace('cm3','').repl
 df.mileage = df.mileage.apply(lambda x :x.replace('km','').replace(' ','')).astype(int)
 df.model = df.model.str.split().str[1]
 
+# create dummy variables for categorical variables
 df = df.join(pd.get_dummies(df.gearbox))
 df = df.join(pd.get_dummies(df.fuel_type))
 df = df.join(pd.get_dummies(df.brand))
 
-# Drop columns 
+# create dummy variables for categorical variables
 df = df.drop('gearbox',axis = 1)
 df = df.drop('fuel_type',axis = 1)
 df = df.drop('brand',axis = 1)
